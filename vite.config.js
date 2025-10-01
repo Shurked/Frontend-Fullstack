31f56684c479667be249e70d216afd8872f37752
DiegoAlv17 <diegonapa17@gmail.com>
2025-10-01 11:37:53 -0500

    añadiendo config de vite para el preview en deploy

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  preview: {
+    allowedHosts: ['kuska.onrender.com']
+  }
 })
